e91278282d9f8198c454cc5def4959441cdcfdb0
matheus.cavalcanti <matheus.cavalcanti@lavid.ufpb.br>
2024-08-24 21:26:56 -0300

    :construction: adjusting the header

--- src/CountryPrompt.tsx
+++ src/CountryPrompt.tsx
@@ -1,14 +1,24 @@
 import React from "react";
+import { getRandomPastelColor } from "./utils/getRandomColor"; // Import your utility function
 
 interface CountryPromptProps {
   countryToGuess: string | null;
   roundNumber: number;
 }
 
-const CountryPrompt: React.FC<CountryPromptProps> = ({ countryToGuess, roundNumber }) => (
-  <div>
-    <h2>Round {roundNumber}/10: Find {countryToGuess}</h2>
-  </div>
-);
+const CountryPrompt: React.FC<CountryPromptProps> = ({
+  countryToGuess,
+  roundNumber,
+}) => {
+  const backgroundColor = getRandomPastelColor(); // Generate a random pastel color
+
+  return (
+    <div id='header' style={{ backgroundColor}}>
+      <h2 className="header">
+        Round {roundNumber}/10: Find {countryToGuess}
+      </h2>
+    </div>
+  );
+};
 
 export default CountryPrompt;
